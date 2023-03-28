5de18aebf9bbdbbc09f8bd9af7acf3d3cc9c3fc5
Jessicaltw <Jessicaltwing@gmail.com>
2023-03-28 02:27:11 -0400

    ccomponents added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {  BrowserRouter, Route, Routes} from 'react-router-dom';
+import Account from './components/Account';
+import FilmRolls from './components/FilmRolls';
+import Home from './components/Home';
+import Review from './components/Review';
 
 function App() {
   return (
     <div className="App">
-    hello, Filmroll.
+      hello, Filmroll.
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/review" element={<Review />} />
+          <Route exact path="/filmrolls" element={<FilmRolls />} />
+          <Route exact path="/account" element={<Account/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/FilmRolls.js
@@ -0,0 +1,7 @@
+export default function FilmRolls(){
+    return (
+        <div>
+            FILM ROLLS PAGE
+        </div>
+    )
+}
