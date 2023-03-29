5563caa00b73efbddf37d257c0e84d597a062268
Jessicaltw <Jessicaltwing@gmail.com>
2023-03-29 01:26:35 -0400

    add home and menu materials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {  BrowserRouter, Route, Routes} from 'react-router-dom';
 import Account from './components/Account';
 import FilmRolls from './components/FilmRolls';
 import Home from './components/Home';
+import Menu from "./components/Menu";
 import Review from './components/Review';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/filmrolls" element={<FilmRolls />} />
           <Route exact path="/account" element={<Account/>} />
         </Routes>
+        <Menu />
       </BrowserRouter>
     </div>
   );
